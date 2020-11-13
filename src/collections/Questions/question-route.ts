6729fc5f5b6da9e63c1f5921ea6ab669559686f7
NawafSwe <nawaf.jr9@hotmail.com>
2020-11-13 18:20:11 +0300

    making first validator works just fine

--- src/collections/Questions/question-route.ts
+++ src/collections/Questions/question-route.ts
@@ -1,12 +1,21 @@
 import express = require('express');
 import {Request, Response} from "express";
-import {getQuestions, postQuestion, deleteQuestion, putQuestion} from "./question-controller";
 import {QuestionInterface} from "./models/question-interface";
 
+const {validate} = require('./question-validator');
+const {validationResult} = require('express-validator');
+const {getQuestions, postQuestion, deleteQuestion, putQuestion} = require("./question-controller");
+
 export const questionRoute: express.Router = express.Router();
-questionRoute.get('/', async (req: Request, res: Response) => {
-    const response: QuestionInterface[] | undefined = await getQuestions();
-    res.send(response).status(200);
+
+questionRoute.get('/', validate('getQuestions'), async (req: Request, res: Response) => {
+    const err = validationResult(req);
+    if (!err.isEmpty()) {
+        res.send(err.mapped()).status(400);
+    } else {
+        const response: QuestionInterface[] | undefined = await getQuestions();
+        res.send(response).status(200);
+    }
 });
 
 questionRoute.post('/', async (req: Request, res: Response) => {
