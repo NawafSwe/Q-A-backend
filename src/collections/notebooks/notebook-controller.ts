4c5f0c8ac150debebeab61e666428b47f435e8c1
NawafSwe <nawaf.jr9@hotmail.com>
2020-11-15 16:21:24 +0300

    doc: notebook controller

--- src/collections/notebooks/notebook-controller.ts
+++ src/collections/notebooks/notebook-controller.ts
@@ -1,6 +1,35 @@
+/**
+ * this controller requires the following packages
+ * @requires NotebookModel
+ * @requires NotebookInterface
+ */
+
+
+/**
+ * @type {import {NotebookInterface} from"./notebook-interface" }
+ * @namespace NotebookInterface
+ * @description notebook interface needed to specify the type of mongoose document
+ */
 import {NotebookInterface} from "./models/notebook-interface";
+
+
+/**
+ * @type {import {NotebookModel} from"./notebook-model" }
+ * @namespace NotebookModel
+ * @description Notebook model that have all the functions related to database operations
+ */
 import {NotebookModel} from './models/notebook-model';
 
+
+/**
+ * @author Nawaf Alsharqi
+ * @async
+ * @function
+ * @name getNotebooks
+ * @returnsPromise<NotebookInterface[] | undefined | null> | never
+ * @throws {Error} it may throws an error during processing
+ * @description getting all notebooks from database
+ */
 export const getNotebooks: () => Promise<NotebookInterface[] | undefined | null> | never = async () => {
     try {
         const response: NotebookInterface[] | undefined = await NotebookModel.find({});
@@ -9,6 +38,18 @@ export const getNotebooks: () => Promise<NotebookInterface[] | undefined | null>
         console.log(`error occurred in getNotebooks() error: ${error} `)
     }
 };
+
+/**
+ * @author Nawaf Alsharqi
+ * @async
+ * @function
+ * @name postNotebook
+ * @param {NotebookInterface} notebook that holds the notebook data
+ * @returns Promise<NotebookInterface | undefined | null> | never
+ * @throws {Error} it may throws an error during the processing if there is something wrong
+ * @description posting new notebook to database
+ */
+
 export const postNotebook: (notebook: NotebookInterface) => Promise<NotebookInterface | undefined | null> | never = async (notebook) => {
     try {
         const response: NotebookInterface | undefined = await NotebookModel.create(notebook);
@@ -18,6 +59,17 @@ export const postNotebook: (notebook: NotebookInterface) => Promise<NotebookInte
         console.log(`error occurred in the postNotebook() error: ${error}`);
     }
 };
+
+/**
+ * @author Nawaf Alsharqi
+ * @async
+ * @function
+ * @name getNotebookById
+ * @param {string} id of question
+ * @returns Promise<NotebookInterface | undefined | null> | never
+ * @throws {Error} throws an error if there is an error
+ * @description getting notebook by id
+ */
 export const getNotebookById: (id: string) => Promise<NotebookInterface | undefined | null> | never = async (id) => {
     try {
         const response: NotebookInterface | undefined | null = await NotebookModel.findById(id);
@@ -26,6 +78,19 @@ export const getNotebookById: (id: string) => Promise<NotebookInterface | undefi
         console.log(`error occurred in getNotebookById() error: ${error}`);
     }
 };
+
+
+/**
+ * @author Nawaf Alsharqi
+ * @async
+ * @function
+ * @name deleteNotebookById
+ * @param {string} id
+ * @returns Promise<NotebookInterface | undefined | null> | never
+ * @throws {Error} it may throws an error during the processing if there is an error
+ * @description deleting notebook from the database by id
+ */
+
 export const deleteNotebookById: (id: string) => Promise<NotebookInterface | undefined | null> | never = async (id) => {
 
     try {
@@ -36,6 +101,17 @@ export const deleteNotebookById: (id: string) => Promise<NotebookInterface | und
     }
 };
 
+/**
+ * @author Nawaf Alsharqi
+ * @async
+ * @function
+ * @name putQuestion
+ * @param {string} id
+ * @param {NotebookInterface} notebook
+ * @returns Promise<NotebookInterface | undefined | null> | never
+ * @throws {Error} throws an error if there is an error during the process
+ * @description update notebook data by id from database
+ */
 export const putNotebookById: (id: string, notebook: NotebookInterface) => Promise<NotebookInterface | undefined | null> | never = async (id, notebook) => {
     try {
         const response: NotebookInterface | undefined | null = await NotebookModel.findByIdAndUpdate(id, notebook);
