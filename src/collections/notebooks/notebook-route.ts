b42990be434c19e60ec9d47a24fff9c6b1c7376f
NawafSwe <nawaf.jr9@hotmail.com>
2020-11-15 04:33:54 +0300

    feature: covring all validators for notebook

--- src/collections/notebooks/notebook-route.ts
+++ src/collections/notebooks/notebook-route.ts
@@ -18,6 +18,15 @@ notebookRoute.get('/', validate('getNotebooks'), async (req: Request, res: Respo
     }
 
 });
+notebookRoute.post('/', validate('postNotebook'), async (req: Request, res: Response) => {
+    const err: any = validationResult(req);
+    if (!err.isEmpty()) {
+        res.send(err.mapped()).status(400);
+    } else {
+        const response: NotebookInterface | undefined | never | null = await postNotebook(req.body);
+        res.send(response).status(200);
+    }
+});
 
 
 notebookRoute.get('/:id', validate('getNotebookById'), async (req: Request, res: Response) => {
@@ -30,17 +39,22 @@ notebookRoute.get('/:id', validate('getNotebookById'), async (req: Request, res:
     }
 });
 
-notebookRoute.delete('/:id', async (req: Request, res: Response) => {
-    const response: NotebookInterface | undefined | null | never = await deleteNotebookById(req.params.id);
-    res.send(response).status(200);
+notebookRoute.delete('/:id', validate('deleteNotebookById'), async (req: Request, res: Response) => {
+    const err: any = validationResult(req);
+    if (!err.isEmpty()) {
+        res.send(err.mapped()).status(400);
+    } else {
+        const response: NotebookInterface | undefined | null | never = await deleteNotebookById(req.params.id);
+        res.send(response).status(200);
+    }
 });
 
-notebookRoute.post('/', async (req: Request, res: Response) => {
-    const response: NotebookInterface | undefined | never | null = await postNotebook(req.body);
-    res.send(response).status(200);
-});
-
-notebookRoute.put('/:id', async (req: Request, res: Response) => {
-    const response: NotebookInterface | undefined | null | never = await putNotebookById(req.params.id, req.body);
-    res.send(response).status(200);
+notebookRoute.put('/:id', validate('putNotebook'), async (req: Request, res: Response) => {
+    const err: any = validationResult(req);
+    if (!err.isEmpty()) {
+        res.send(err.mapped()).status(400);
+    } else {
+        const response: NotebookInterface | undefined | null | never = await putNotebookById(req.params.id, req.body);
+        res.send(response).status(200);
+    }
 });
